c3e11c81bfb810d7a3b535b8f829aabae365e2fb
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2018-06-12 10:28:11 +0200

    Add JWT integration

--- api/permissions.py
+++ api/permissions.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -5,4 +6,9 @@ class AllowOptionsAuthentication(IsAuthenticated):
     def has_permission(self, request, view):
         if request.method == 'OPTIONS':
             return True
+
+        if getattr(request, 'session', None) and \
+                request.session.get('jwt_iss') == settings.JWT_ALLOWED_ISSUER:
+            return True
+
         return request.user and request.user.is_authenticated
