83606217c346342f96aa7c9084e3bb9bd597fbd6
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2018-06-26 16:23:44 +0200

    Make trailing slash optional for URLs

---
+++ api/tests/test_routers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+
+class RouterTest(TestCase):
+    def test_get_request_does_not_redirect(self):
+        response = self.client.options('/api/documents/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_request_does_not_redirect_without_slash(self):
+        response = self.client.options('/api/documents')
+        self.assertEqual(response.status_code, 200)
