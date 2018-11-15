75552a3e4c1ddc64324c97991b8d172b5394867a
Greg Lind <gwlind@gmail.com>
2018-11-15 17:29:26 +0100

    add static root

--- documents-service/settings/base.py
+++ documents-service/settings/base.py
@@ -29,6 +29,7 @@ DEBUG = False if os.getenv('DEBUG') == 'False' else True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATIC_ROOT = '/static/'
 
 STATIC_URL = '/static/'
 
